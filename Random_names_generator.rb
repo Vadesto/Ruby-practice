2e97a5e252cdd3d0c81c34d46a57c87eabc29ca8
Vladislav Karpushkin <vadeesto@gmail.com>
2020-04-07 19:31:17 +0700

    no more "string". The output is generated right away

--- Random_names_generator.rb
+++ Random_names_generator.rb
@@ -5,7 +5,7 @@ adjectives = ["minty", "yummy", "fluffy", "filthy", "jumbo", "fluffy", "embarass
 nouns = ["starship", "advice", "rattlesnake", "pancake", "orange", "regret", "insurance", "ukulele", "carol", "noun"]
 
 while 1 == 1
-  string = [adjectives.sample, nouns.sample, rand(100)]
-  puts string.join("-")
+  puts [adjectives.sample, nouns.sample, rand(100)].join("-")
+
   gets
 end
