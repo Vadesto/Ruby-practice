4e3c00d160ccba663938a3cdf13efd44a965d431
Vladislav Karpushkin <vadeesto@gmail.com>
2020-04-07 18:04:35 +0700

    Changed the working method. NOw you can press enter multiple times to get different results. Stops by 'ctrl + c'

--- Random_names_generator.rb
+++ Random_names_generator.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
-adjectives = ["Lemon-flavored", "Minty", "Yummy", "Fluffy", "Filthy", "Jumbo", "Big-boned", "Fluffy", "Embarassed", "Confused", "Grumpy"]
+adjectives = ["Lemon-flavored", "Minty", "Yummy", "Fluffy", "Filthy", "Jumbo", "Bigboned", "Fluffy", "Embarassed", "Confused", "Grumpy"]
 nouns = ["Army", "Advice", "Rattlesnake", "Pancake", "Orange", "Regret", "Insurance", "Ukulele", "Carol", "Noun"]
-string = adjectives.sample.downcase + "-" + nouns.sample.downcase + "-" + rand(100).to_s
-puts string
+while 1 == 1
+  i = gets.chomp
+  if i == ""
+    string = adjectives.sample.downcase + "-" + nouns.sample.downcase + "-" + rand(100).to_s
+    puts string
+  end
+end
